8b0537629078dc0e5f4bdb61167b67c67260b324
icanul <icanul@icanul.royalresorts.local>
2019-10-24 18:01:46 -0500

    se agrega componente ace on React

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
 import React from 'react';
 import './scss/App.scss';
-import Logo from './img/logo192.png';
+import AceEditor from "react-ace";
 
 class App extends React.Component{
   constructor(props){
     super(props);
-    console.log(Logo);
     
   }
+  componentDidMount(){
+
+
+  }
+  save = ()=>{
+
+  }
   render(){
-  return (
-    <div className="App">
-      <header>
-        <img src={Logo} alt="Logo"/>
-      </header>
-      <div className="contenedor">
-        <ul className="actions">
-          <li className="actions-action"><a href="/">Descargars</a></li>
-          <li className="actions-action"></li>
-          <li className="actions-action"></li>
-        </ul>
-      </div>
-      <div className="contenedor">
-        <div id="editorJS">Editor JS</div>
-        <div id="editorCSS">Editor Css</div>
-      </div>
-      <div className="contenedor">
-        <div id="editorHTML">editor html</div>
-        <div id="contenedor result">
+    return (
+      <div className="App">
+        <header>
+          NAV
+        </header>
+        <div className="contenedor">
+          <div id="editorJS">Editor JS</div>
+          <div id="editorCSS">Editor Css</div>
+        </div>
+        <div className="contenedor">
+          <ul className="tabs">
+            <li className="noList tabs__tab"><a href="/" className="tab noLink btn">CSS</a></li>
+            <li className="noList tabs__tab"><a href="/" className="tab noLink btn">HTMl</a></li>
+          </ul>
+        </div>
+        <div class="contenedor result">        
+          <div id="editorHTML" className="active">
+            <AceEditor
+              mode="javascript"
+              theme="monokai"
+              name="UNIQUE_ID_OF_DIV"
+              editorProps={{ $blockScrolling: true }}
+            />
+          </div>
           <div className="result__wrapper">
             resultado
           </div>
         </div>
+        <div className="contenedor">
+          <ul className="actions">
+            <li className="actions__action noList"><a className="noLink btn" href="/">Descargar</a></li>
+            <li className="actions__action noList"></li>
+            <li className="actions__action noList"></li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
